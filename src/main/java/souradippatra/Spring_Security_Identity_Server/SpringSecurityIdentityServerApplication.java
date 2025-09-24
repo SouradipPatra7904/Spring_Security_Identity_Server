a224a4f6d3923d8454c7fb3dfa0a40944e5265f6
Souradip Patra <spectralphoenix.dev@gmail.com>
2025-09-24 13:20:14 +0530

    New Levels, New Devils.

---
+++ src/main/java/souradippatra/Spring_Security_Identity_Server/SpringSecurityIdentityServerApplication.java
@@ -0,0 +1,13 @@
+package souradippatra.Spring_Security_Identity_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityIdentityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityIdentityServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/souradippatra/Spring_Security_Identity_Server/SpringSecurityIdentityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package souradippatra.Spring_Security_Identity_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityIdentityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
